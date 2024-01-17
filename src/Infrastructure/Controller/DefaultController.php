ff7a77f4ba5ee8215e47a441b032c108e6e084e3
Daniel González <daniel@devtia.com>
2024-01-17 17:07:23 +0100

    [#15] - Desarrollo de una interfaz de línea de comandos
    * Configure web server

---
+++ src/Infrastructure/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Infrastructure\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: '_app.default')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ConferenceController.php',
+        ]);
+    }
+}
